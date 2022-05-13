7c002ac9614ea4ea29c38ccd172faba4e64a28df
gabitodev <gabitodev@gmail.com>
2022-05-13 17:10:22 -0400

    app, index and controllers created

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const logger = require('./utils/logger');
+const { MONGO_URI } = require('./utils/config');
+const blogRouter = require('./controllers/blog');
+const mongoose = require('mongoose');
+
+mongoose.connect(MONGO_URI)
+  .then(() => {
+    logger.info('Connected to MongoDB');
+  })
+  .catch(error => {
+    logger.error('Error conecting to MongoDB', error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/blogs', blogRouter);
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,34 +1,10 @@
+const app = require('./app');
 const http = require('http');
-const express = require('express');
-const app = express();
-const cors = require('cors');
+const { PORT } = require('./utils/config');
 const logger = require('./utils/logger');
-const { MONGO_URI, PORT } = require('./utils/config');
-const Blog = require('./models/blog');
 
-mongoose.connect(MONGO_URI);
+const server = http.createServer(app);
 
-app.use(cors());
-app.use(express.json());
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    });
-});
-
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
+server.listen(PORT, () => {
+  logger.info(`Server running in port ${PORT}`);
 });
